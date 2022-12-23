bddde9eaee4c98dcf9b19e00e3b693c7b391947a
SyedaAnoosha <anushazaidi06@gmail.com>
2022-12-23 16:35:14 +0500

    initial

--- src/lab_13_tasks/Main.java
+++ src/lab_13_tasks/Main.java
@@ -1,5 +1,15 @@
 package lab_13_tasks;
 
+/*
+
+download json-simple jar file before running code  https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/json-simple/json-simple-1.1.1.jar
+
+ File > Project Structure > Libraries > + > Java > json simple folder
+
+ */
+
+
+
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
